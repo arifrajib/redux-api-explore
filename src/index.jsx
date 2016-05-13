82beab79d8d2997ed062ec76c54a0a42e447eb13
Ariful Islam Rajib <arif.rajib@gmail.com>
2016-05-12 17:08:36 -0700

    Define Action Creator

--- src/index.jsx
+++ src/index.jsx
@@ -7,3 +7,24 @@ Define Action Type
 const PUSH_ACTION = 'PUSH';
 const POP_ACTION = 'POP';
 
+
+/***
+Define Action Creator
+***/
+function pushActionCreator(item){
+	console.log("Creating Push Action inside 'pushActionCreator'");
+	return {
+		type: PUSH_ACTION,
+		item
+	};
+}
+
+function popActionCreator(){
+	console.log("Creating Push Action inside 'popActionCreator'");
+	return {
+		type: POP_ACTION
+	};
+}
+
+
+
